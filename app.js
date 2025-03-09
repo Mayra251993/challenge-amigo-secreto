731e2dc71ef1a2025530089c25b78b8d7c3a257d
Mayra Altamirano <altamiranomayra3@gmail.com>
2025-03-09 01:10:08 -0300

    Actualizo el apps.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigosIngresados = [];
 
+let amigos = []
 
-function agregarAmigo() {
-    let amigosIngresados = document.getElementById("nombre").value.trim(); // Elimina espacios en blanco
-
-    if (amigosIngresados === "") {
-        alert("Por favor, inserte un nombre."); // Mensaje de error
-    } else {
-        alert("El nombre ingresado es: " + nombre);
-    }
+function agregarAmigo(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
+
 
