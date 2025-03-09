4c7742f83e12291088ab7c79dc2a47be6ddbf3b0
Mayra Altamirano <altamiranomayra3@gmail.com>
2025-03-08 23:51:34 -0300

    Agrego en app.js el Array que almacena los amigos ingresados

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigosIngresados = [3];
+
+
+function agregarAmigo() {
+    console.log("Amigo agregado!");
+}
+
